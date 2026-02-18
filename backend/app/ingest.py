425af4434a005c690316754c0cbb5b5aa1bca402
Ridho Chan <147796339+RidhoChan04@users.noreply.github.com>
2026-02-18 11:35:07 +0700

    perbaikan model embeddings

--- backend/app/ingest.py
+++ backend/app/ingest.py
@@ -31,7 +31,7 @@ ID_KEY = "doc_id"
 # VECTORSTORE + RETRIEVER
 # =========================
 embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004",
+    model="models/embedding-001",
     google_api_key=os.getenv("GEMINI_API_KEY")
 )
 
